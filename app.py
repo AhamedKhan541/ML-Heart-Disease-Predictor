618bb00b91e2dd748279eccfda882891155ed2a1
Pathan Mohammed Ahamed Khan <pathanahamedk@gmail.com>
2025-04-19 17:09:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def predict():
         return render_template('index.html', prediction_text="Error: " + str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=5000)
